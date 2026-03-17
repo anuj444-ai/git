c0bb73883f4cf29206c7efb5e170199d4e9f3fec
Anuj Dubey <dubeyanuj4444@gmail.com>
2026-03-17 20:35:46 +0530

    my code

---
+++ 1progam.py
@@ -0,0 +1,1082 @@
+# print("Hello, World!")
+# print("anuj is my name")
+# print("my age is 20") 
+# print("23")
+# print("23+23")
+
+# name = "Anuj"
+# age = 20
+# price = 23.5
+
+# age2 = age
+
+# print("name")
+# print("age")
+# print("price")
+
+# print("my name is :", name)
+# print("my age is :", age)
+# print("the price is :", price)
+
+# print("my age is :", age2)
+
+
+
+# name = "Anuj"
+# age = 20    
+# price = 23.5
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# name1 = "Anuj"
+# name2 ='Anuj'
+# name3 = """Anuj"""
+
+# print(name1)
+# print(name2)
+# print(name3)
+
+# age = 20
+# old = false
+# a= None
+# print(type(old))
+# print(type(a))
+# print(type(age))
+
+# a = 2
+# b = 3
+# sum = a + b
+# print("sum")
+# print(sum)
+
+# a = 1000
+# b = 2000
+# sum = a + b
+# print("sum")
+
+# print(sum)
+
+# a =100000000
+# b = 200000000
+# sum =a - b
+# print("sum")
+# print(sum)
+
+
+# a = 100000000
+# b = 200000000
+# diff = a - b
+# print("diff")   
+# print(diff)
+
+
+# print("hello world")
+# print("hello world")
+# """
+# multi-line
+# comment
+# """
+
+# #arithmetic operators
+# a = 10
+# b = 3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) #remainder
+# print(a**b) #a^b
+
+# # relational operators
+# a = 10
+# b = 20
+# print(a == b) #flase
+# print(a != b) #true
+# print(a > b) #false
+# print(a < b) #true
+# print(a >= b) #false
+# print(a <= b) #true
+
+# #assignment operators
+# num = 10
+# num = num + 10
+# print("num:", num)
+# num += 10
+# print("num:", num)
+# num -= 5
+# print("num:", num)
+# num *= 2
+# print("num:", num)
+# num /= 5
+# print("num:", num)
+# num %= 3
+# print("num:", num)
+# num = 10
+# num **= 2
+# print("num:", num) #1,00,00
+
+#  #logical operators
+# print(not True) #false
+# print(not False) #true
+# a = 10
+# b = 20
+# print(a > 5 and b < 30) 
+# print(a > 15 or b < 30) 
+
+# val1 = 10
+# val2 = 20   
+# print(val1 > 5 and val2 < 30)
+# print(val1 > 15 or val2 < 30) 
+
+# print("AND operator:", val1 and val2)
+# print("OR operator:",(a==b) or (a>b))
+
+# #type conversion
+# a = 10
+# b = 3.14
+# sum = a + b # 10 + 3.14 = 13.14
+# print(sum)
+
+# a = int("10")
+# b = 3.14
+# print(type(a))
+# print(a + b) 
+
+# a = float("10")
+# b = 3.14
+# print(type(a))
+# print(a + b)
+
+# a = 3.14
+# a = str(a)
+# print(type(a))
+
+# name = input("Enter your name: ")
+# print("welcome", name)
+
+# age = input("Enter your age: ")
+# print("you entered", age)
+
+# val = input("enter some values: ")
+# print(type(val), val) # "25" , "99.99"
+
+# int("5")
+# val = int(input("enter an values: "))
+# print(type(val), val) 
+
+# int("5")
+# val = float(input("enter an values: "))
+# print(type(val), val) 
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# marks = input("Enter your marks: ")
+
+# print("welcome", name)
+# print("your age is", age)
+# print("your marks are", marks)
+
+# #wap to input 2 numbers & print their sum
+# first= float(input("Enter first number: "))
+# second = float(input("Enter second number: "))    
+# sum = (first + second)
+# print("the sum is:", sum)
+
+# # wap to input side of a square & print its area
+# side = float(input("Enter the side of a square: "))
+# area = side * side
+# print("the area of the square is:", area)
+
+# # wap to  input 2 floating point numbers & print their average
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))    
+# average = (a + b) / 2
+# print("the average is:", average)
+
+#  #wap to input 2 int numbers ,a and b .print true if a is greather than or equal to b . if not print false
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print(a >= b)
+
+
+
+
+## lecture 2-two py
+
+# str1 = "this is a string"
+# str2 = 'global college'
+# str3 = """this is a string"""
+
+# 'this is a global college"s tutorials'
+
+# str1 = "this is a string.\nwe are learning python"
+# print(str1)
+
+
+# str1 = "global"
+# str2 = "college"
+# print(str1+str2)
+
+# str1 = "global"
+# str2 = "college"
+# final_str = str1+str2
+# print(final_str)
+
+# str1 = "global"
+# len1 = len(str1)
+# print("length of str1:", len1)
+
+# str2 = "college"
+# len2 = len(str2)
+# print("length of str2:", len2)
+
+# final_str = str1 + str2
+# print(final_str)
+
+
+# str1 = "global"
+# len1 = len(str1)
+# print("length of str1:", len1)
+
+# str2 = "college"
+# len2 = len(str2)    
+# print("length of str2:", len2)
+
+# final_str = str1 +""+ str2
+# print(final_str)
+# print("length of final_str:", len(final_str))
+
+
+# str = "global college"
+# ch = str[0]
+# print(ch)
+
+# str = "global college"
+# ch = str[7]
+# print(ch)
+
+# str = "global college"
+# print(str[1:5])
+
+# str = "global college"
+# print(str[0:14])
+
+# str = "global college"
+# print(str[:6])
+# print(str[7:])
+
+# str = "bannana"
+# print(str[-5:-2])
+
+# str = "bannana"
+# print(str[-6:])
+
+# str = "bannana"
+# print(str[:-1])
+
+# str = "i am learning python"
+# print(str.endswith("python"))
+
+
+# str = "i am learning python"
+# print(str.capitalize())
+# print(str)
+
+# str = "i am learning python"
+# print(str.replace("python", "java"))
+# print(str)
+
+# str = "i am learning python"
+# print(str.find("ear"))
+
+
+# str = "i am learning python"
+# print(str.find("java"))
+
+# str = "i am learning python"
+# print(str.count("n"))
+
+
+# # wap to input user's first name & print its length
+# first_name = input("Enter your first name: ")
+# print("length of your first name is:", len(first_name))
+
+# #wap to find the occurance of '&' in the string.
+# str = "Hi, $ I am Anuj $ I am learning python & I am enjoying it."
+# print(str.count("$"))
+
+# age = 20
+
+# if (age >= 18):
+#     print("can vote & apply for driving license")
+
+
+# age = 16
+
+# if (age >= 18):
+#     print("can vote & apply for driving license")
+
+
+# age = 25
+
+# if (age >= 18):
+#     print("can vote & apply for driving license")
+
+
+# age = 15
+
+# if (age >= 18):
+#     print("can vote & apply for driving license")#true / false
+#     print("can vote")
+#     print("can drive")
+
+# light = "green"
+
+# if (light == "green"):
+#     print("go")
+# elif (light == "yellow"):
+#    print("ready")
+# elif (light == "red"):
+#    print("stop")
+# else:
+#     print("light is broken")   
+
+# print("end of the program")
+
+# num = 10
+
+# if (num > 2):
+#     print("greater than 2")
+# if (num > 5):
+#     print("greater than 5")
+# if (num > 10):
+#     print("greater than 10")
+
+# age = 14
+
+# if (age >= 18):
+#     print("can vote")
+# else:
+#     print("cannot vote")
+
+# marks = int(input("Enter your marks: "))
+
+# if (marks >= 90):
+#     print("grade A")
+# elif (marks >= 80 and marks < 90):
+#     print("grade B")
+# elif (marks >= 70 and marks < 80):
+#     print("grade C")
+# else:
+#     print("grade D")            
+
+# age = 34
+
+# #nesting
+# if (age >= 18):
+#     if (age >=80):
+#         print("cannot driving ")
+#     else:
+#         print("can driving ")
+# else:
+#     print("cannot drive")
+
+
+# # wap to check a number & check if it is even or odd
+# num = int(input("Enter a number: "))
+# if (num % 2 == 0):
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+
+# #wap to find the greatest of 3 numbers entered by the user
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: ")) 
+# if (num1 >= num2 and num1 >= num3):
+#     print("The greatest number is:", num1)
+# elif (num2 >= num1 and num2 >= num3):
+#     print("The greatest number is:", num2)
+# else:
+#     print("The greatest number is:", num3)
+
+# # wap to check if a number is a multiple of 7 or not
+# num = int(input("Enter a number: "))
+# if (num % 7 == 0):
+#     print("The number is a multiple of 7")
+# else:
+#     print("The number is not a multiple of 7")
+
+
+# # lecture 3- three py
+
+# marks1 = 95.2
+# marks2 = 88.5
+# marks3 = 76.0
+# marks4 = 82.3
+# marks5 = 91.7
+
+# marks = [95.2, 88.5, 76.0, 82.3, 91.7]
+# print(marks)
+# print(type(marks))
+
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+# students = ["Anuj", 98.1, 17, "delhi"]
+# print(students)
+# students[0] = "Rahul"
+# print(students)
+
+# marks = [95.2, 88.5, 76.0, 82.3, 91.7]
+# print(marks[1:4])
+# print(marks[-3:-1])
+
+# list= [1, 2, 3]
+# list.append(4)
+# print(list)
+
+# list = [1, 2, 3]
+# list.append(4)
+# print(list.sort())
+# print(list)
+
+# list = ["bannana", "apple", "grape"]
+# #print(list.append(4))  
+# print(list.sort(reverse=True))
+# print(list)
+
+# list = ['a', 'b', 'c' ]
+# #print(list.append(4))
+# print(list.sort(reverse=True))
+# print(list)
+
+# list = ['a','d','f','g','t','w','k','h','x']
+# list.reverse()
+# print(list)
+
+# list = [1, 2, 3, 4, 5]
+# list.insert(2, 10)
+# print(list)
+
+
+# list = [2, 1, 3, 1]
+# list.remove(1)
+# print(list)
+
+# list = [2, 1, 3, 1]
+# list.pop(1)
+# print(list)
+
+# tup = (1, 2, 3, 4, 5)
+# print(type(tup))
+
+# tup = (1,)
+# print(tup)
+# print(type(tup))
+
+# tup = (1, 2, 3)
+# print(tup)
+# print(type(tup))
+
+# tup = (1, 2, 3, 4)
+# print(tup[1:3])
+
+# tup = (1, 2, 3, 4, 2, 2)
+# print(tup.count(2))
+
+
+# # wap to ask user to enter name of their 3 favorite movies & store them in a list. 
+# movies = []
+# mov1 = input("Enter the name of your first favorite movie: ")
+# mov2 = input("Enter the name of your second favorite movie: ")
+# mov3 = input("Enter the name of your third favorite movie: ")   
+
+# movies.append(mov1)
+# movies.append(mov2) 
+# movies.append(mov3)
+
+# print("movies:", movies)
+
+
+# #wap two check if a list contains a palindrome of element . (hint: usecopy()method)
+# list1 = ["madam", "hello", "world", "level", "python"]
+# list2 = ["madam", "level"]
+
+# copy_list = list1.copy()
+# copy_list.reverse()
+
+# if(copy_list == list1):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+
+# #wap to count the number of stiudents with the "A" grade in the following tuple.
+
+# grades = ("A", "B", "A", "C", "A")
+# count = grades.count("A")
+# print("Number of students with grade A:", count)
+
+# #store the above values in a list & sort them from "A" to "D"
+
+# grade = ["A", "B", "A", "C", "A"]
+# grade.sort()
+# print("Sorted grades:", grade)
+
+# info = {   
+
+
+#     "key": "value",
+#     "name": "anuj",
+#     "learning": "python",
+#     "age": 20
+    
+
+# }
+
+# print(info)
+
+# info = {
+#     "name": "anuj",
+#     "learning": ["python", "java", "c++"],
+#     "age": 20,
+#     "is_student": True,
+#     "marks": (95.2, 88.5, 76.0, 82.3, 91.7)
+
+
+# }
+# print(info)
+
+
+# info = {
+#     "name": "global college",
+#     "subject": ["python", "c"],
+#     "age": 35,
+# }
+
+# info = ["name"] = "anuj"
+# info = ["surname"] = "dubey"
+# print(info)
+
+
+# info = {
+#     "name": "global college",
+#     "subjects": ["python", "c", "c++"],
+#     "topics": ("dict","set"),
+#     "age": 35,
+#     "is_adult": True,
+#     12.99 : 94.54
+# }
+
+# print(type(info))
+
+# info = {
+#      "name": "global college",
+#      "subjects": ["python", "c", "c++"],
+#     "topics": ("dict","set"),
+#     "age": 35,
+#      "is_adult": True,
+#        12.99 : 94.54
+#  }
+
+# print(info["name"])
+# print(info["topics"])
+# print(info["subjects"])
+# print(info["age"])
+
+
+# null_dict = {}
+# null_dict = ["name"] = "global college"
+# print(null_dict)
+
+
+# student = {
+#     "name": "anujdubey",
+#     "subjects": {
+#         "phy" : 97,
+#         "chem" : 98,
+#         "math" : 95,
+#     }
+# }
+# #nested dictionary
+# print(student["subjects"]["chem"])
+
+# student = {
+#     "name": "anujdubey",
+#      "subjects": {
+#         "phy" : 97,
+#         "chem" : 98,
+#         "math" : 95,
+#      }
+# }
+
+# print(student.keys())
+# print(list(student.keys()))
+
+# student = {
+#     "name": "anujdubey",
+#      "subjects": {
+#         "phy" : 97,
+#         "chem" : 98,
+#         "math" : 95,
+#      }
+# }
+
+# print(list(student.values()))
+
+# student = {
+#     "name": "anujdubey",
+#      "subjects": {
+#         "phy" : 97,
+#         "chem" : 98,
+#         "math" : 95,
+#      }
+# }
+# print(student.items())
+# print(list(student.items()))
+ 
+# student = {
+#     "name": "anujdubey",
+#      "subjects": {
+#         "phy" : 97,
+#         "chem" : 98,
+#         "math" : 95,
+#       }
+#  }
+# print(student["name"])
+# print(student.get("name"))
+
+# student = {
+#     "name": "anujdubey",
+#      "subjects": {
+#         "phy" : 97,
+#         "chem" : 98,
+#         "math" : 95,
+#       }
+# }
+# student.update({"city":"delhi"})
+# print(student)
+
+# student = {
+#     "name": "anujdubey",
+#      "subjects": {
+#         "phy" : 97,
+#         "chem" : 98,
+#         "math" : 95,
+#       }
+# }
+# new_dict = {"city":"delhi", "age": 16}
+# student.update(new_dict)
+# print(student)
+
+# collection = {1, 2, 3, 4, "hello", "world"}
+
+# print(collection)
+# print(type(collection))
+# print(len(collection))
+
+# collection = {} #empty dictionary
+
+# print(type(collection))
+
+# collection = set() #empty set; syntax
+
+# print(type(collection))
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(2)
+
+# print(collection)
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(2)
+
+# collection.remove(2)
+# print(collection)
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(2)
+# collection.add("global college")
+# collection.add((1, 2, 3))
+
+# print(len(collection))
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(2)
+# collection.add("global college")
+# collection.add((1, 2, 3))
+
+# collection.clear()
+# print(len(collection))
+
+# collection = {"hello", "globalcollege", "world", "coding", "python"}
+
+# print(collection.pop())
+# print(collection.pop())
+# print(collection.pop())
+# print(collection.pop())
+# print(collection.pop())
+
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 4}
+
+# print(set1.union(set2)) #{1, 2, 3, 4}
+# print(set1)
+# print(set2)
+
+# # store following word meaning in a python  dictionary
+# #table : "a piece of furnitire","list of facts & figures"
+# #cat : "a small animal"
+
+# dict = {
+#     "cat" : "a small animal",
+#     "table" : ["a piece of furniture", "list of facts & figures"]
+# }
+
+# print(dict)
+
+# subjects = {
+#     "python", "java", "python", "javascript", "java", 
+#     "python", "java", "c++", "c"
+# }
+
+# print(subjects)
+
+# marks = {}
+
+# x = int(input("enter phy : "))
+# marks.update({"phy" : x})
+
+# x = int(input("enter math : "))
+# marks.update({"math" : x})
+
+# x = int(input("enter chem : "))
+# marks.update({"chem" : x})
+
+# print(marks)
+
+# values = {9, 9.0}
+# print(values)
+
+# values = {
+#     ("float", 9.0),
+#     ("int", 9)
+# }
+
+# while True :
+#     print("hello")
+
+
+# count = 1
+# while count <= 5 :
+#     print("hello")
+#     count += 1
+
+# count = 1
+# while count <= 5 :
+#     print("hello")
+#     count += 1
+    
+
+# i = 1
+# while i <= 5:
+#     print("hello")
+#     i+=1    
+
+#print numbers from 1 to 5
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# print("loop ended")  
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# print("loop ended")  
+
+# i = 5
+# while i <= 6:
+#     print(i)
+#     i -= 1
+
+# print("loop ended")  
+   
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+
+# i = 100
+# while i >= 1: #stopping coding
+#      print(i)
+#      i -= 1
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# i = 1
+# while i <= 100:
+#     print(100000*i)
+#     i += 1
+
+# n = int(input("enter number : "))
+# i = 1 
+# while i<= 10:
+#     print(n*i)
+#     i +=1
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx]) # nums[0], nums[1], nums[2]..
+#     idx += 1
+
+# heros = ["ironman", "thor", "superman", "batman"]
+
+# idx = 0
+# while idx < len(heros):
+#     print(heros[idx])
+#     idx += 1
+
+# nums = (1, 2, 3, 34, 45, 56, 67, 78, 89, 100, 36)
+
+# x = 36
+
+# i= 0
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found at idx", i)
+#     else:
+#         print("finding..")
+#     i += 1
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if(i == 3):
+#         break
+#     i += 1
+
+# print("end of loop")
+
+
+# nums = (1, 2, 3, 34, 45, 56, 67, 78, 89, 100, 36)
+
+# x = 36
+
+# i= 0
+# while i < len(nums):
+#    if(nums[i] == x):
+#         print("found at idx", i)
+#         break
+#    else:
+#        print("finding..")
+#    i += 1
+
+# print("end of loop")
+
+
+# i = 0
+# while i<= 5:
+#     if(i == 3):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# i = 0
+# while i<=   10:
+#    if(i%2 == 0):
+#         i += 1
+#         continue
+#    print(i)
+#    i += 1
+
+
+# i = 0
+# while i<=   10:
+#    if(i%2 != 0):
+#         i += 1
+#         continue
+#    print(i)
+#    i += 1
+
+# nums = [1, 2, 3, 4, 5, 6]
+
+# for val in nums:
+#     print(val)
+
+# veggies = ["potato", "brinjal", "ladyfinger", "cucumber"]
+
+# for val in veggies:
+#     print(val)
+    
+# tup = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+# for num in tup:
+#     print(num)
+
+# str = "globalcollege"
+
+# for char in str:
+#     print(char)
+
+# str = "globalcollege"
+
+# for char in str:
+#     print(char)
+# else:
+#     print("END")
+
+# str = "globalcollege"
+
+# for char in str:
+#     if(char == 'o'):
+#         print("o found")
+#         break
+#     print(char)
+
+# else:
+#     print("END")
+        
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# for el in nums:
+#     print(el)
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49]
+# x = 49
+
+# idx = 0
+# for el in nums:
+#     if(el == x):
+#         print("number found at idx", idx)
+#     idx += 1
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49]
+# x = 49
+
+# idx = 0
+# for el in nums:
+#     if(el == x):
+#         print("number found at idx", idx)
+#         break
+#     idx += 1
+
+
+# seq = range(5)
+
+# for i in seq:
+#     print(i)
+
+# seq = range(10)
+
+# for i in seq:
+#     print(i)
+    
+# for i in range(10): #range(stop)
+#     print(i)
+
+# for i in range(2, 10):  #range(stop)
+#     print(i)
+
+# for i in range(10): #range(stop)
+#     print(i)
+
+# for i in range(2, 10):  #range(start, stop)
+#     print(i)
+
+# for i in range(2, 10, 2): #range(start, stop, step)
+#     print(i)
+
+# for i in range(2, 100, 2):
+#     print(i)
+
+# for i in range(1, 101, 2):
+#     print(i) 
+
+# for i in range(1, 101):
+#     print(i)
+    
+# for i in range(100, 0, -1):
+#     print(i)
+    
+# n = int(input("enter number :"))
+
+# for i in range(1, 11):
+#     print(n*i)
+
+
+# for i in range(5):
+#     pass
+
+# print("some useful work")
+
+# for i in range(5):
+#     pass
+
+# if i > 5:
+#     pass
+
+# print("some useful work")
+
+# n = 5
+
+# for i in range(1, n+1):
+#     print(i)
+
+# n = 5 
+
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+
+# print("total sum =", sum)
+
+# n = 7
+# sum = 0
+# i = 1
+# while i <= n:
+#     sum += i
+#     i += 1
+
+# print("total sum =", sum)
+
+# n = 5 
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+
+# print("factorial =", fact)
+
+# n = 5
+# fact = 1
+
+# for i in range(1, n+1):
+#      fact *= i
+
+# print("factorial =", fact) 
+
+
